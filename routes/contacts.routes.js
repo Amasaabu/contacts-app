4308a5136c55081817d62ec435689e9416c1aa45
Abubakar Amasa <amasaabubakar@gmail.com>
2025-02-17 13:23:00 +0000

    search-complete

--- routes/contacts.routes.js
+++ routes/contacts.routes.js
@@ -31,7 +31,53 @@ router.get('/contacts', async(req, res) => {
 })
 
 
+//get a contact by firstName
+router.get('/contacts/firstname/:firstName', async(req, res) => {
+    const firstName = req.params.firstName;
+    try {
+        let contacts = await getAllContact();
+        let contact = contacts.filter(c => c.firstName === firstName);
+        if (contact.length === 0) {
+            res.status(404).send({message: 'Contact not found'});
+        } else {
+            res.send(contact);
+        }
+    } catch (error) {
+        res.status(500).send('Error getting contact')
+    }
+})
 
+//get contact by lastname
+router.get('/contacts/lastname/:lastName', async(req, res) => {
+    const lastName = req.params.lastName;
+    try {
+        let contacts = await getAllContact();
+        let contact = contacts.filter(c => c.lastName === lastName);
+        if (contact.length === 0) {
+            res.status(404).send({message: 'Contact not found'});
+        } else {
+            res.send(contact);
+        }
+    } catch (error) {
+        res.status(500).send('Error getting contact')
+    }
+})
+
+//get contact by id
+router.get('/contacts/id/:id', async(req, res) => {
+    const id = req.params.id;
+    try {
+        let contacts = await getAllContact();
+        let contact = contacts.filter(c => c.id === id);
+        if (contact.length === 0) {
+            res.status(404).send('Contact not found');
+        } else {
+            res.send(contact);
+        }
+    } catch (error) {
+        res.status(500).send('Error getting contact')
+    }
+})
 
 //delete a contact
 router.delete('/contacts/:id', (req, res) => {
